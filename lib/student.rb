5e5f88e4a705c8f4d009dd3b4b8b8e4046668c93
Meghan Flynn <mflynn333@gmail.com>
2020-02-28 22:58:21 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -35,6 +35,12 @@ class Student
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
   end 
   
+  def self.create (name:, grade:)
+    new_student = Student.new(name, grade)
+    new_student.save 
+    new_student
+  end 
+  
 end
 
 
